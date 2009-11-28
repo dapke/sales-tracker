bb306e78531aea6aff2d2ebb78bd212c2420a51c
james.elsey <james.elsey@c3ac1864-cbad-11de-b393-33af5e5426e5>
2009-11-28 19:01:01 +0000

    Added more data to prospect domain class, also made sure data is real for better testing purposes. Also checked in viewProspect action/jsp because I forgot on the last checkin

--- trunk/src/main/java/com/jameselsey/salestracker/domain/Prospect.java
+++ trunk/src/main/java/com/jameselsey/salestracker/domain/Prospect.java
@@ -6,6 +6,14 @@ public class Prospect
     private Integer id;
     private String name;
     private String sector;
+    private String address1;
+    private String address2;
+    private String town;
+    private String county;
+    private String postcode;
+    private String country;
+    private String websiteURL;
+    private String mainPhoneNumber;
 
     public Integer getId()
     {
@@ -36,4 +44,86 @@ public class Prospect
     {
         this.sector = sector;
     }
+
+    public String getAddress1()
+    {
+        return address1;
+    }
+
+    public void setAddress1(String address1)
+    {
+        this.address1 = address1;
+    }
+
+    public String getAddress2()
+    {
+        return address2;
+    }
+
+    public void setAddress2(String address2)
+    {
+        this.address2 = address2;
+    }
+
+    public String getCountry()
+    {
+        return country;
+    }
+
+    public void setCountry(String country)
+    {
+        this.country = country;
+    }
+
+    public String getCounty()
+    {
+        return county;
+    }
+
+    public void setCounty(String county)
+    {
+        this.county = county;
+    }
+
+    public String getMainPhoneNumber()
+    {
+        return mainPhoneNumber;
+    }
+
+    public void setMainPhoneNumber(String mainPhoneNumber)
+    {
+        this.mainPhoneNumber = mainPhoneNumber;
+    }
+
+    public String getTown()
+    {
+        return town;
+    }
+
+    public void setTown(String town)
+    {
+        this.town = town;
+    }
+
+    public String getWebsiteURL()
+    {
+        return websiteURL;
+    }
+
+    public void setWebsiteURL(String websiteURL)
+    {
+        this.websiteURL = websiteURL;
+    }
+
+    public String getPostcode()
+    {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode)
+    {
+        this.postcode = postcode;
+    }
+
+    
 }
