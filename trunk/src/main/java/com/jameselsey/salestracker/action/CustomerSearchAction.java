8ede355819608a4737df206b19fd8853e458272e
james.elsey <james.elsey@c3ac1864-cbad-11de-b393-33af5e5426e5>
2009-11-30 20:07:43 +0000

    refactored a little bit, moved prospects from dashboard over to the customer search page since its more relevant there

--- trunk/src/main/java/com/jameselsey/salestracker/action/CustomerSearchAction.java
+++ trunk/src/main/java/com/jameselsey/salestracker/action/CustomerSearchAction.java
@@ -1,5 +1,8 @@
 package com.jameselsey.salestracker.action;
 
+import com.jameselsey.salestracker.domain.Prospect;
+import com.jameselsey.salestracker.service.ProspectService;
+import java.util.List;
 import net.sourceforge.stripes.action.DefaultHandler;
 import net.sourceforge.stripes.action.ForwardResolution;
 import net.sourceforge.stripes.action.Resolution;
@@ -11,11 +14,17 @@ import net.sourceforge.stripes.action.Resolution;
 public class CustomerSearchAction extends BaseAction
 {
 
+    ProspectService prospectService = new ProspectService();
+
+    public List<Prospect> getProspects()
+    {
+        return prospectService.fetchProspects();
+    }
+
     @DefaultHandler
     public Resolution randomDate()
     {
         return new ForwardResolution(VIEW);
     }
-
     private static final String VIEW = "/jsp/customerSearch.jsp";
 }
